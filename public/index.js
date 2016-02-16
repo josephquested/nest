e4229a19975586505e20b54bf10ce3882aef0d5d
Joseph Quested <josephquested@icloud.com>
2016-02-16 18:50:50 +1300

    Add (finally) working build)

--- public/index.js
+++ public/index.js
@@ -1,10 +1,10 @@
-import ajax from './ajax.js'
+import ajax from '../lib/ajax.js'
 
 export default function () {
-  ajax.getTables(returnData)
-  ajax.getPlayers(returnData)
+  // ajax.getTables(returnData)
+  // ajax.getPlayers(returnData)
 }
 
 const returnData = (data) => {
-  console.log('returned data: ', JSON.parse(data))
+  // console.log('returned data: ', JSON.parse(data))
 }
